aa2e7e2e246187fcb57d0f2a0cf64306a3ad5465
Akira <156126180+akiraonstarknet@users.noreply.github.com>
2025-03-09 16:18:59 +0530

    chore: Ensure installed wallets are seen on top first

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -100,6 +100,18 @@ export function getConnectors(isMobile: boolean) {
     },
   });
 
+  const isInstalled = [argentXConnector, braavosConnector, keplrConnector].map(
+    (wallet) => {
+      return {
+        id: wallet.id,
+        isInstalled:
+          typeof window === 'undefined'
+            ? false
+            : window[`starknet_${wallet.id}`] !== undefined,
+      };
+    },
+  );
+
   const webWalletConnector = new WebWalletConnector({
     url: 'https://web.argent.xyz',
   }) as StarknetkitConnector;
@@ -116,13 +128,33 @@ export function getConnectors(isMobile: boolean) {
       webWalletConnector,
     ];
   }
-  return [
+
+  const defaultConnectors = [
     argentXConnector,
     braavosConnector,
     keplrConnector,
-    mobileConnector,
-    webWalletConnector,
   ];
+
+  // put uninstall wallets at the end
+  const sortedConnectors: any[] = defaultConnectors.sort((a, b) => {
+    const aInstalled = isInstalled.find(
+      (wallet) => wallet.id === a.id,
+    )?.isInstalled;
+    const bInstalled = isInstalled.find(
+      (wallet) => wallet.id === b.id,
+    )?.isInstalled;
+
+    if (aInstalled && bInstalled) {
+      return 0;
+    } else if (aInstalled) {
+      return -1;
+    }
+    return 1;
+  });
+
+  sortedConnectors.push(mobileConnector);
+  sortedConnectors.push(webWalletConnector);
+  return sortedConnectors;
 }
 
 interface NavbarProps {
